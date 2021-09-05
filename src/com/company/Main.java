ce43dad0811a459812feaf04beb2e6fed1ce1367
annewynter <kushnareva86@gmail.com>
2021-09-04 20:12:59 -0400

    updated classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,11 @@ public class Main {
 
         System.out.println("hello");
 
-        int num=0;
-        int num2=num=10;
+        int num = 0;
+        int num2 = num = 10;
         System.out.println(num2);
 
-
-
-
+        String str_Sample = "RockStar";
+        System.out.println("EndsWith character 'r': " + str_Sample.endsWith("r"));
     }
 }
